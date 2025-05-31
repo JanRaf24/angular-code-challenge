8c427389c1d174af7d40fbe7f22a521e5480dd83
Jan Etienne Rafols <jrafols@tastradesoft.com>
2025-05-31 10:55:41 +0800

    feat: set up routing, breadcrumbs, and array deduplication utility
    - Configured routing using @angular/router for list and user details pages
    - Implemented breadcrumb service and components to display navigation context
    - Modified AppComponent and AppModule to integrate breadcrumb functionality
    - Added array utility function to remove duplicates based on a given object key

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+
+const routes: Routes = [
+  { 
+    path: '',
+    data: { breadcrumb: 'List'},
+    children: [
+      { 
+        path: '', component: UsersListComponent ,
+      },
+      {
+        path: ':id', component: UserDetailsComponent, 
+        data: { breadcrumb: 'Profile'} 
+      }
+    ]
+  },
+  { path : '**' , redirectTo : '' , pathMatch : 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/utils/array.ts
@@ -0,0 +1,11 @@
+export function removeDuplicates<T>(arr: T[], keySelector: (item: T) => string): T[] {
+  const seen = new Set<string>();
+  return arr.filter(item => {
+    const key = keySelector(item);
+    if (seen.has(key)) {
+      return false;
+    }
+    seen.add(key);
+    return true;
+  });
+}
