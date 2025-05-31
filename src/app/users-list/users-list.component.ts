3638238ec79ae555d53528143d327726e8db8a60
Jan Etienne Rafols <jrafols@tastradesoft.com>
2025-05-31 11:09:37 +0800

    refactor: update UsersList component to standalone and enhance styling
    - Converted UsersList component to standalone and imported CommonModule
    - Updated SCSS: added .card-container class with shadow and hover effects
    - Modified component template to apply new styles

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,10 +1,13 @@
 import {Component} from '@angular/core';
-import {UserService} from '../services/user.service';
+import {UserService} from 'src/app/services/user.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.scss']
+  styleUrls: ['./users-list.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class UsersListComponent {
   public readonly users$ = this.userService.getAll();
