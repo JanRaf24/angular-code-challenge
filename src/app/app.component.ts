461ba05a18fb93e5dcb30b7fc8bf8fa98c1f735b
Jan Etienne Rafols <jrafols@tastradesoft.com>
2025-06-01 00:34:40 +0800

    feat: enhance AppComponent layout with Bootstrap styling and custom container
    - Wrapped Breadcrumbs component and RouterOutlet in a styled div container
    - Applied Bootstrap utility classes for container, margin, padding, shadow, and rounded corners
    - Created AppComponent SCSS and added .app_container class to set width to 50vw and height to 80vh
    - Improves overall visual structure and layout consistency across the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['./app.component.scss'],
   template: `
-    <app-breadcrumb></app-breadcrumb>
-    <div class="container py-4 px-3 mx-auto">
-      <router-outlet></router-outlet>
+    <div class="container my-4 p-0 shadow rounded mx-auto overflow-auto app-container">
+      <app-breadcrumb></app-breadcrumb>
+      <div class="container py-4 px-3 mx-auto">
+        <router-outlet></router-outlet>
+      </div>
     </div>
   `
 })
