ac29e993e21e85a992d96c8aa0987de4c096c3ed
Jan Etienne Rafols <jrafols@tastradesoft.com>
2025-06-01 00:18:20 +0800

    feat: enhance Breadcrumbs component with animations and updated styling
    - Integrated slideInRight and zoomIn animations on route change
    - Updated HTML structure and applied Bootstrap utility classes for shadow, padding, and margin
    - Modified SCSS to adjust font-size for improved readability

--- src/app/components/breadcrumb/breadcrumb.component.ts
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BreadcrumbService, Breadcrumb } from '../../services/breadcrumb/breadcrumb.service';
 import { Observable } from 'rxjs';
+import { slideInRightTrigger, zoomInTrigger } from 'src/app/animations';
 
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
-  styleUrls: ['./breadcrumb.component.scss']
+  styleUrls: ['./breadcrumb.component.scss'],
+  animations: [zoomInTrigger(), slideInRightTrigger()],
 })
 export class BreadcrumbComponent implements OnInit {
   @Input() separator: string = '>'; // Default separator
@@ -16,7 +18,6 @@ export class BreadcrumbComponent implements OnInit {
 
   ngOnInit(): void {
     this.breadcrumbs$ = this.breadcrumbService.getBreadcrumbs();
-    this.breadcrumbService.getBreadcrumbs().subscribe(bc => console.log(bc))
   }
 
 }
